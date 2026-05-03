985ec634ab76785795166b09447e58c90096a942
Benjamin de la fuente <206235917+Ibenjamindlf@users.noreply.github.com>
2026-05-03 16:25:42 -0300

    Update Home.jsx
    Integro y limpio home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,21 @@
 import HeroSection from '../../components/Hero/heroSection';
-import { ProductCard } from '../../components/ProductCard/ProductCard';
 import FilterBar from '../../components/FilterBar/FilterBar';
 import { useWatches } from '../../hooks/useWatches';
 import { ProductGrid } from '../../components/ProductGrid/ProductGrid';
+import { InfiniteScrollTrigger } from '../../components/InfiniteScrollTrigger/InfiniteScrollTrigger';
 
 const Home = () => {
-
-  const { watches, loading, searchQuery, loadMore } = useWatches();
+  const { watches, loading, loadMore } = useWatches();
 
   return (
-    <>
-    {/* Contenedor principal para que tome el color oscuro de tu paleta */}
     <div className="bg-primary min-h-screen">
       <HeroSection />
+      
       <div className="max-w-7xl mx-auto px-4 py-12 flex flex-col items-center">
         
         <div className="w-full mb-8 border-b border-secondary/30 pb-4">
           <h2 className="text-3xl font-serif text-contrast">
-            Catálogo <span className="text-sm font-sans text-accent tracking-widest uppercase ml-2">(Vista Temporal)</span>
+            Catálogo <span className="text-sm font-sans text-accent tracking-widest uppercase ml-2">(Exploración Infinita)</span>
           </h2>
         </div>
 
@@ -27,20 +25,15 @@ const Home = () => {
         
         <ProductGrid watches={watches} />
 
-        {/* Todo esto podria ser un componente ? */}
+
         {watches.length > 0 && (
-            <button 
-                onClick={loadMore} 
-                disabled={loading} 
-                className="mt-12 px-8 py-3 bg-accent text-primary font-semibold rounded hover:bg-accent/80 disabled:opacity-50 transition-colors cursor-pointer"
-            >
-                {loading ? "Buscando piezas..." : "Descubrir más"}
-            </button>
-          )}
-        {/* Hasta aca */}
+          <InfiniteScrollTrigger 
+            onTrigger={loadMore} 
+            loading={loading} 
+          />
+        )}
       </div>
     </div>
-  </>
   );
 };
 
