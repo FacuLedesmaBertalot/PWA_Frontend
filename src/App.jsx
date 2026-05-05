6aa74f7c5c021eafec232871599fcc5a8e67f457
Benjamin de la fuente <206235917+Ibenjamindlf@users.noreply.github.com>
2026-05-05 11:26:36 -0300

    Update App.jsx
    Actualizo App.jsx incluyendo la ruta destino de /favorites, de esta
    forma podemos navegar entre las paginas, las 3 rutas son de caracter
    provisorio, nose si se van a modificar o van a quedar tal cual estan.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
 import { Details } from './pages/Details/Details'; 
 import { ProductNotFound } from './pages/ProductNotFound/ProductNotFound'; 
+import { Favorites } from './pages/Favorites/Favorites';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             {/* Rutas temporales para poder visualizar los cambios del feature #31 */}
             <Route path="/item/:id" element={<Details />} />
             <Route path="*" element={<ProductNotFound />} />
+            <Route path="/favorites" element={<Favorites />} />
             {/* Fin rutas temporales */}
           </Routes>
         </main>
