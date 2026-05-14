2c85081973d21d9e6df0a6b5c9c7c1ff0ff85dd5
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-05-14 11:19:12 -0300

    Fixed error de sintaxis

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     tailwindcss(),
   ],
   test: { 
-    enviroment: 'jsdom',
+    environment: 'jsdom',
     globals: true,
     setupFiles: './src/setupTests.js'
   }
