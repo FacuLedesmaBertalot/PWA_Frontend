d897a0ec69f4b51a6af93f651d75af0514398e52
FacuLedesmaBertalot <fanelebe.contacto@gmail.com>
2026-06-29 19:19:37 -0300

    feat: integrar AuthProvider en App

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,30 @@ import { Details } from './pages/Details/Details';
 import { ProductNotFound } from './pages/ProductNotFound/ProductNotFound'; 
 import { Favorites } from './pages/Favorites/Favorites';
 import { FavoritesProvider } from './context/FavoritesContext';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <BrowserRouter>
-      <FavoritesProvider>
-        <div className="flex flex-col min-h-screen">
-          <Navbar />
-          <main className="grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/registro" element={<Signup />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/items/:id" element={<Details />} />
-              <Route path="/favoritos" element={<Favorites />} />
+      <AuthProvider>
+        <FavoritesProvider>
+          <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <main className="grow">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/registro" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/items/:id" element={<Details />} />
+                <Route path="/favoritos" element={<Favorites />} />
 
-              <Route path="*" element={<ProductNotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </FavoritesProvider>
+                <Route path="*" element={<ProductNotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </FavoritesProvider>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
